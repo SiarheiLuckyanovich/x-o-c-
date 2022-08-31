a730c2a6f8efef572aefe79d98fe6da498d97d7a
Siarhei Lukyanovich <codename_arhey@mail.ru>
2022-08-31 23:08:33 +0300

    prewinproblem

--- X/main.cpp
+++ X/main.cpp
@@ -35,6 +35,8 @@ struct TGame
     TProgress progress { IN_PROGRESS };
     TCell human, ai;
     size_t turn { 0U };
+    char chouseDificalt;
+    char m, e;
 };
 #pragma pack(pop)
 //=========================================================================
@@ -132,11 +134,11 @@ TProgress __fastcall getWon(const TGame & g)
   //победа в столбцах
     for (size_t x=0; x < g.SIZE; x++)
     {
-         if ((g.ppField [x][0]== g.ppField [x][1]) && (g.ppField [x][0] == g.ppField [x][2]))
+         if ((g.ppField [0][x]== g.ppField [1][x]) && (g.ppField [0][x] == g.ppField [2][x]))
         {
-            if (g.ppField [x][0] == g.human)
+            if (g.ppField [0][x] == g.human)
                 return WON_HUMAN;
-            if (g.ppField [x][0] == g.ai)
+            if (g.ppField [0][x] == g.ai)
                 return WON_AI;
         }
     }
@@ -208,7 +210,7 @@ TCoord __fastcall getHumanCoord(const TGame & g)
 
 }
 //=========================================================================
-TCoord __fastcall getAICoord(TGame & g)
+TCoord __fastcall getAICoordH(TGame & g)
 {
 //предвыйгрышь
  for (size_t y = 0; y < g.SIZE; y++)
@@ -309,17 +311,176 @@ TCoord __fastcall getAICoord(TGame & g)
  return { 0, 0};
 }
 
+TCoord __fastcall getAICoordM(TGame & g)
+{
+//предпроигрыш
+ for (size_t y = 0; y < g.SIZE; y++)
+    {
+        for (size_t x = 0; x < g.SIZE; x++)
+        {
+            if (g.ppField [y][x] == EMPTY)
+            {
+                g.ppField [y][x] = g.human;
+                if (getWon(g) == WON_HUMAN)
+                {
+                    g.ppField [y][x] = EMPTY;
+                    return { y, x};
+                }
+                g.ppField [y][x] = EMPTY;
+            }
+        }
+    }
+//преоритеты
+    if (g.ppField[1][1] == EMPTY)//центр
+    {
+        return { 1, 1};
+        g.ppField [1][1] = EMPTY;
+    }
+
+
+    TCoord buf [4];//углы
+    size_t num {0U};
+    if (g.ppField[0][0] == EMPTY)
+    {
+         buf[num] = { 0 , 0};
+         num++;
+    }
+    if (g.ppField[0][2] == EMPTY)
+    {
+         buf[num] = { 0 , 2};
+         num++;
+    }
+    if (g.ppField[2][0] == EMPTY)
+    {
+         buf[num] = { 2 , 0};
+         num++;
+    }
+    if (g.ppField[2][2] == EMPTY)
+    {
+         buf[num] = { 2 , 2};
+         num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = getRandomNum( 0, 1000) %num;
+        return buf[index];
+    }
+    //не углы
+
+    if (g.ppField[0][1] == EMPTY)
+    {
+         buf[num] = { 0 , 1};
+         num++;
+    }
+    if (g.ppField[1][0] == EMPTY)
+    {
+         buf[num] = { 1 , 0};
+         num++;
+    }
+    if (g.ppField[1][2] == EMPTY)
+    {
+         buf[num] = { 1 , 2};
+         num++;
+    }
+    if (g.ppField[2][1] == EMPTY)
+    {
+         buf[num] = { 2 , 1};
+         num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = getRandomNum( 0, 1000) %num;
+        return buf[index];
+    }
+ return { 0, 0};
+}
+
+TCoord __fastcall getAICoordE(TGame & g)
+{
+//преоритеты
+    if (g.ppField[1][1] == EMPTY)//центр
+    {
+        return { 1, 1};
+        g.ppField [1][1] = EMPTY;
+    }
+
+
+    TCoord buf [4];//углы
+    size_t num {0U};
+    if (g.ppField[0][0] == EMPTY)
+    {
+         buf[num] = { 0 , 0};
+         num++;
+    }
+    if (g.ppField[0][2] == EMPTY)
+    {
+         buf[num] = { 0 , 2};
+         num++;
+    }
+    if (g.ppField[2][0] == EMPTY)
+    {
+         buf[num] = { 2 , 0};
+         num++;
+    }
+    if (g.ppField[2][2] == EMPTY)
+    {
+         buf[num] = { 2 , 2};
+         num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = getRandomNum( 0, 1000) %num;
+        return buf[index];
+    }
+    //не углы
+
+    if (g.ppField[0][1] == EMPTY)
+    {
+         buf[num] = { 0 , 1};
+         num++;
+    }
+    if (g.ppField[1][0] == EMPTY)
+    {
+         buf[num] = { 1 , 0};
+         num++;
+    }
+    if (g.ppField[1][2] == EMPTY)
+    {
+         buf[num] = { 1 , 2};
+         num++;
+    }
+    if (g.ppField[2][1] == EMPTY)
+    {
+         buf[num] = { 2 , 1};
+         num++;
+    }
+    if (num > 0)
+    {
+        const size_t index = getRandomNum( 0, 1000) %num;
+        return buf[index];
+    }
+ return { 0, 0};
+}
+
+
+
 //=========================================================================
 int main()
 {
- TGame g;
- initGame(g);
- system("cls");
- printGame(g);
- char chouseDificalt;
- cout << "                       Choose the difficulty (H-hard, M-medium, E - easy): ";
-        cin >> chouseDificalt;
- do {
+char answ;
+do {
+    TGame g;
+
+    initGame(g);
+    system("cls");
+    printGame(g);
+
+
+    cout << "             Choose the difficulty (H-hard, M-medium, E - easy): ";
+    cin >> g.chouseDificalt;
+
+
+    do {
         system("cls");
         printGame(g);
         if (g.turn %2 == 0)
@@ -329,18 +490,40 @@ int main()
         }
         else
         {
-        const TCoord c = getAICoord(g);
-        g.ppField[c.y][c.x] = g.ai;
+            if (g.chouseDificalt == 'm')
+            {
+            cout << "           So, you choose m-medium ;). Enjoy!";
+            const TCoord c = getAICoordM(g);
+            g.ppField[c.y][c.x] = g.ai;
+            } else if (g.chouseDificalt == 'e')
+            {
+            cout << "           So, you choose e-easy ;). Enjoy!";
+            const TCoord c = getAICoordE(g);
+            g.ppField[c.y][c.x] = g.ai;
+            }
+            else
+            {
+            cout << "           So, you choose h-hard ;). Enjoy!";
+            const TCoord c = getAICoordH(g);
+            g.ppField[c.y][c.x] = g.ai;
+            }
+
         }
         system("cls");
         printGame(g);
         g.turn++;
         g.progress = getWon(g);
 
+
         } while (g.progress == IN_PROGRESS);
 
-  deinitGame(g);
-  congrats(g);
-  return 0;
+    deinitGame(g);
+    congrats(g);
+
+    cout << "            Do you wanna play again? (say yes): ";
+    cin >> answ;
+    } while (answ == 'y');
+
+return 0;
 }
 //=========================================================================
